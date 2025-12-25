2e26eeae2720563d70066065d0aca44d185e04ef
ba <172867949+ba172@users.noreply.github.com>
2025-12-25 04:00:57 +0300

    Update check.php

--- check.php
+++ check.php
@@ -3,17 +3,19 @@ header("Content-Type: application/json");
 header("X-Content-Type-Options: nosniff");
 header("X-Frame-Options: DENY");
 
-$input = $_POST['input'] ?? '';
+// استقبال الكود
+$input = $_POST['input'] ?? '';  // الكود المدخل من قبل المستخدم
 
+// تحقق: يجب أن يكون 8 أرقام
 if (!preg_match('/^\d{8}$/', $input)) {
-    echo json_encode(["status" => "denied", "message" => "wrong code"]);
+    echo json_encode(["status" => "denied", "message" => "CODE N'T WORK"]);
     exit;
 }
 
 // قراءة الأكواد من Environment Variable
 $codes_env = getenv("CODES_JSON");
 if (!$codes_env) {
-    echo json_encode(["status" => "error", "message" => "code no't found"]);
+    echo json_encode(["status" => "error", "message" => " ERROR "]);
     exit;
 }
 
@@ -21,7 +23,7 @@ $codes = json_decode($codes_env, true);
 
 // تحقق من الكود
 if (!isset($codes[$input])) {
-    echo json_encode(["status" => "denied", "message" => "code no't work !! ]);
+    echo json_encode(["status" => "denied", "message" => "FAILD CODE !!!"]);
     exit;
 }
 
@@ -29,7 +31,7 @@ if (!isset($codes[$input])) {
 $emails_env = getenv("EMAILS_TXT");
 $emails = $emails_env ? explode("\n", $emails_env) : [];
 
-// إرجاع JSON
+// إرجاع الإيميلات للمستخدم في JSON
 echo json_encode([
     "status" => "allowed",
     "count"  => count($emails),
